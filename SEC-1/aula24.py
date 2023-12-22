95bad6e3f58d65489fb42fab0ad2f12b7e78a3f8
Lucas Lima <lima1301lucas@gmail.com>
2023-12-22 09:46:53 -0300

    Aula 24-26

---
+++ SEC-1/aula24.py
@@ -0,0 +1,13 @@
+nome = 'Lucas'
+sobrenomenome = 'Lima'
+idade = 20
+ano_nascimento = 2003
+maior_de_idade = True
+altura_metros = 1.85
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de nascimento:', ano_nascimento)
+print('É maior de idade?', maior_de_idade)
+print('Altura em metros:', altura_metros)
